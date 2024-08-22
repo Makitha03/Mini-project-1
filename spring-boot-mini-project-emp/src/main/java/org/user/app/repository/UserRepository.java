0bb45a23ebb9c4b431d6b1ea293add02ac2e3a33
Makitha03 <makitha.s0304@gmail.com>
2024-08-22 20:12:04 +0530

    Delete spring-boot-mini-project-emp directory

--- spring-boot-mini-project-emp/src/main/java/org/user/app/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.user.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.user.app.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
