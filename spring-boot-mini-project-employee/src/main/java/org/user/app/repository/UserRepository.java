8f501fce0586f6c545c493b06ff44034c90e2501
Makitha03 <makitha.s0304@gmail.com>
2024-08-26 08:13:24 +0530

    Delete spring-boot-mini-project-employee directory

--- spring-boot-mini-project-employee/src/main/java/org/user/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.user.app.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.user.app.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-
-}
